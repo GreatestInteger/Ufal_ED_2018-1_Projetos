4186dd28633a15d24e64fe7e770894b542c8c2e0
Guilherme <32186360+GreatestInteger@users.noreply.github.com>
2018-10-04 22:08:08 -0300

    Reorganizing headers and files

--- Huffman/1_main.c
+++ Huffman/1_main.c
@@ -1,15 +1,19 @@
-//#include "6_util.h"
-#include "2_compress.h"
-//#include "3_decompress.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "4_queue_and_huffman_tree.c"
+#include "3_hash_table.c"
+#include "2_compress.c"
 
 int main(){
 
 	int choice;
 	char source_file_name[100];
-    char destination_file_name[100];
+  	char destination_file_name[100];
 
 	printf("\n Choose an option:\n\n\t1 - Compress a file.\n\t2 - Decompress a huffman file.\n\n\t");
 	scanf("%d", &choice);
+
 	switch(choice){
 
 		case 1:
@@ -24,10 +28,11 @@ int main(){
             scanf("%s", source_file_name);
             printf("\n\tDESTINATION (uncrompressed) file name: ");
             scanf("%s", destination_file_name);
-			decompress(source_file_name, destination_file_name);
+			//decompress(source_file_name, destination_file_name);
 			break;
 		default:
 			printf("\n\tError!\n\n");
 	}
 
-return
+return 0;
+}
--- Huffman/2_compress.c
+++ Huffman/2_compress.c
@@ -1,6 +1,30 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
+void freq_count(void *file, hash_table *ht){
+
+	unsigned char *buffer;
+	int i;
+
+	fseek(file , 0 , SEEK_END); // Ajusta a posição do indicador com base em uma referência. SEEK_END (final do arquivo) é a referência e 0 é posição à partir da referência. Isso significa que o indicador vai permanecer na referência. O resultado dessa função é que o indicador está ajustado para indicar o final do arquivo.
+
+	int file_size = ftell(file); // A função "ftell" retorna a posição do indicador. Nesse caso, ele retorna a posição ajustada pela "fseek", ou seja, o valor da posição do último caractere do arquivo. Essa posição é interpretada como o tamanho do arquivo em bytes (1 caractere = 1 byte).
+
+	rewind(file); // Essa função ajusta o indicador para o começo do arquivo, como era antes da função "fseek".
+
+	buffer = (unsigned char *) malloc(sizeof(char) *file_size); // Faz uma atribuição ao "buffer" reservando um espaço na memória igual ao tamanho do arquivo.
+
+	fread(buffer, 1, file_size, file); // Copia os caracteres do arquivo para o "buffer".
+
+	//ht = &(HashT*) ht;
+
+	for(i = 0; i < file_size; i++){
+
+		ht -> table[buffer[i]] -> frequency++;
+
+		// Percorre o buffer e atualiza (incrementa) a frequencia do caractere encontrado na hash table.
+	}
+
+	free(buffer);
+
+} // Calcula a frequência de cada caractere e armazena as frequências na hash table.
 
 
 void compress(char *source_file_name, char *destination_file_name){
@@ -9,7 +33,8 @@ void compress(char *source_file_name, char *destination_file_name){
 	hash_table *hasht = create_hash_table();
 	node  *queue = create_empty_queue();
 	node  *huffman_tree = create_empty_tree();
-	int i;
+	unsigned int i;
+	//unsigned char data[256];
 
 	source_file = fopen(source_file_name, "rb");
 	compressed_file = fopen(destination_file_name, "wb");
@@ -26,14 +51,17 @@ void compress(char *source_file_name, char *destination_file_name){
 
 
 	for(i = 0; i < 256; i++){
-		if(hasht -> table[i] -> freq > 0){
-			queue = create_node(queue, i, hasht -> table[i] -> freq);
+		if(hasht -> table[i] -> frequency > 0){
+			//data[i] = i;
+			queue = enqueue(queue, &i, hasht -> table[i] -> frequency);
 		}
 	}
+	//print_priority_queue(queue);
 
 	huffman_tree = huff_tree(queue);
 
-
+	print_tree(huffman_tree);
+	/*
 	fprintf(compressed_file, "00");
 
 	unsigned char bit_string[255];
@@ -70,6 +98,7 @@ void compress(char *source_file_name, char *destination_file_name){
 	fclose(compressed_file);
 
 	printf("\n\tEnd of compression!\n\n");
+	*/
 }
 
 int getUniqueBit(unsigned int c, int i){
